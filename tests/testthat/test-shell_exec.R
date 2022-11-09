a39dc34a00ec73eb6db99d2b172151f25d0e71b4
luciorq <luciorqueiroz@gmail.com>
2022-11-09 01:02:37 -0500

    chore: add pkgdown config

--- tests/testthat/test-shell_exec.R
+++ tests/testthat/test-shell_exec.R
@@ -1,4 +1,5 @@
 test_that("Execute shell commands", {
+  testthat::skip_on_os("windows")
   expect_equal(
     shell_exec("R -s -q -e 'base::cat(base::version$version.string)'")$stdout,
     base::version$version.string
