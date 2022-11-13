d7dbed1d40f098be838999e7e4e733c4744cc609
heronoh <heronoh@gmail.com>
2022-11-13 13:32:24 -0300

    corrigindo chamada recursiva db_path, corrigindo total_cores (pro future) e num_threads (por blast), corrigindo perc_id por perc_ID

--- R/parallel_blast.R
+++ R/parallel_blast.R
@@ -8,7 +8,7 @@
 #'
 #' @param asvs Vector of sequences to be BLASTed
 #' @param out_file Path to output file
-#' @param out_rds Path to output RDS file
+#' @param out_RDS Path to output RDS file
 #'
 #' @inheritParams run_blast
 #'
@@ -17,50 +17,51 @@ parallel_blast <- function(
   asvs,
   db_path,
   out_file,
-  out_rds,
+  out_RDS,
   num_thread,
-  perc_id,
+  total_cores,
+  perc_ID,
   perc_qcov_hsp
 ){
   # TODO: Convert ASVs to vector, if needed
-  if (is.null(db_path)) {
-    db_path <- getOption(
-      "BLASTr.db_path",
-      default = NULL
-    )
-  }
-  if (is.null(db_path)) {
-    cli::cli_abort(
-      message = "No BLAST database provided."
-    )
-  }
-  if (is.null(num_thread)) {
-    num_thread <- getOption(
-      "BLASTr.num_thread",
-      default = 1
-    )
-  }
-  if (num_thread > 1) {
-    future::plan(future::multisession(), workers = num_thread)
-    blast_res <- furrr::future_map_dfr(
-      asvs,
-      get_blast_results,
-      num_thread = 1,
-      .options = furrr::furrr_options(seed = NULL),
-      # .progress = TRUE,
-      db_path = db_path,
-      perc_id = perc_id,
-      perc_qcov_hsp = perc_qcov_hsp
-    )
-  } else {
-    blast_res <- purrr::map_dfr(
-      asvs,
-      get_blast_results,
-      num_thread = 1,
-      db_path = db_path,
-      perc_id = perc_id,
-      perc_qcov_hsp = perc_qcov_hsp
-    )
+
+
+  # if (is.null(db_path)) {
+  #   db_path <- getOption(
+  #     "BLASTr.db_path",
+  #     default = NULL
+  #   )
+  # }
+  # if (is.null(db_path)) {
+  #   cli::cli_abort(
+  #     message = "No BLAST database provided."
+  #   )
+  # }
+  # if (is.null(num_thread)) {
+  #   num_thread <- getOption(
+  #     "BLASTr.num_thread",
+  #     default = 1
+  #   )
+  # }
+  if (total_cores > 1) {
+    future::plan(future::multisession(), workers = total_cores)
+    blast_res <- furrr::future_map_dfr(.x = asvs,
+                                       .f = get_blast_results,
+                                       .options = furrr::furrr_options(seed = NULL),
+                                       num_thread = 1,
+                                       # .progress = TRUE,
+                                       db_path = db_path,
+                                       perc_ID = perc_ID,
+                                       perc_qcov_hsp = perc_qcov_hsp
+                                       )
+    } else {
+      blast_res <- purrr::map_dfr(.x = asvs,
+                                  .f = get_blast_results,
+                                  num_thread = 1,
+                                  db_path = db_path,
+                                  perc_ID = perc_ID,
+                                  perc_qcov_hsp = perc_qcov_hsp
+                                )
   }
   if (!is.na(out_file)) {
     readr::write_csv(
@@ -69,10 +70,10 @@ parallel_blast <- function(
       append = FALSE
     )
   }
-  if (!is.na(out_rds)) {
+  if (!is.na(out_RDS)) {
     readr::write_rds(
       x = blast_res,
-      file = out_rds
+      file = out_RDS
     )
   }
   return(blast_res)
