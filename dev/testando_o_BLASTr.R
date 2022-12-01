77ff91e8f0a48760431849d59fd965b0c8200dd9
heronoh <heronoh@gmail.com>
2022-12-01 20:58:28 -0300

    finalizei o benchmark

--- dev/testando_o_BLASTr.R
+++ dev/testando_o_BLASTr.R
@@ -4,7 +4,11 @@ load_all(path = "/home/heron/prjcts/omics/metaseqs/BLASTr")
 
 ASVs_test <- c("CTAGCCATAAACTTAAATGAAGCTATACTAAACTCGTTCGCCAGAGTACTACAAGCGAAAGCTTAAAACTCATAGGACTTGGCGGTGTTTCAGACCCAC",
                "CTAGCCATAAACTTAAATGAAGCTATACTAAACTCGTTCGCCAGAGTACTACAAGTGAAAGCTTAAAACTCATAGGACTTGGCGGTGTTTCAGACCCAC",
-               "GCCAAATTTGTGTTTTGTCCTTCGTTTTTAGTTAATTGTTACTGGCAAATGACTAACGACAAATGATAAATTACTAATAC")
+               "GCCAAATTTGTGTTTTGTCCTTCGTTTTTAGTTAATTGTTACTGGCAAATGACTAACGACAAATGATAAATTACTAATAC",
+               "AACATTGTATTTTGTCTTTGGGGCCTGGGCAGGTGCAGTAGGAACTTCACTTAGAATAATTATTCGTACTGAGCTTGGGCATCCAGGAAGACTTATCGGGGATGATCAAATCTATAATGTAATTGTTACAGCACATGCATTTGTGATAATTTTTTTTATAGTAATACCTATTATGATT",
+               "ACTATACCTATTATTCGGCGCATGAGCTGGAGTCCTAGGCACAGCTCTAAGCCTCCTTATTCGAGCCGAGCTGGGCCAGCCAGGCAACCTTCTAGGTAACGACCACATCTACAACGTTATCGTCACAGCCCATGCATTTGTAATAATCTTCTTCATAGTAATACCCATCATAATCGGAGGCTTTGGCAACTGACTAGTTCCCCTAATAATCGGTGCCCCCGATATG",
+               "TTAGCCATAAACATAAAAGTTCACATAACAAGAACTTTTGCCCGAGAACTACTAGCAACAGCTTAAAACTCAAAGGACTTGGCGGTGCTTTATATCCAC"
+               )
 
 
 
@@ -24,7 +28,7 @@ blast_res <- BLASTr::parallel_blast(asvs = ASVs_test,
                # blast_cmd = "blastn -db {db_path} -outfmt '6 std qcovhsp' -max_hsps 1 -perc_identity 80 -qcov_hsp_perc 80 -num_threads {as.character(num_thread)} -num_alignments {as.character(num_alignments)}",
                total_cores = 77,
                perc_ID = 80,
-               num_thread = 1,
+               num_threads = 1,
                perc_qcov_hsp = 80,
                num_alignments = 4,
                blast_type = "blastn"
@@ -45,3 +49,23 @@ blast_res1 <- BLASTr::run_blast(asv = ASVs_test[3],
 
 
 teste <- readRDS(file = "/home/heron/prjcts/omics/metaseqs/blast_out.RDS")
+
+
+
+# benchmarking
+
+
+
+## To Run Background Job
+
+# rstudioapi::jobRunScript(path = "heron-otu-blastn.R", workingDir = ".", importEnv = TRUE, exportEnv = "R_GlobalEnv")
+
+# ----
+
+
+
+
+
+
+
+
