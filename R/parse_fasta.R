d5032f46ae4b5fe13b0545c9c1e702efc2fda599
luciorq <luciorqueiroz@gmail.com>
2022-12-06 08:53:56 -0500

    feat: initial implementation of parse_fasta

---
+++ R/parse_fasta.R
@@ -0,0 +1,29 @@
+#' Extract Sequences From FASTA file
+#'
+#' Extract nucleotide or aminoacid sequences from FASTA file.
+#'
+#' @param file_path Path to FASTA file.
+#'
+#' @return vector containing the sequences.
+parse_fasta <- function(file_path) {
+  fasta_file <- readr::read_lines(file_path)
+  if (!fs::file_exists(file_path)) {
+    cli::cli_abort(
+      c(
+        "{.file file_path} do not exist."
+      )
+    )
+  }
+  seqs <- vector()
+  for (line in fasta_file) {
+    if (startsWith(line, "#")) {
+      next
+    }
+    if (startsWith(line, ">")) {
+      next
+    } else {
+      seqs <- c(seqs, line)
+    }
+  }
+  return(seqs)
+}
