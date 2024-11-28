538df663c5583f4a6e8872e89a8c505289eecf4e
luciorq <luciorqueiroz@gmail.com>
2024-11-28 17:55:46 -0500

    fix: missing force argument in install_dependencies

--- R/install_dependencies.R
+++ R/install_dependencies.R
@@ -1,6 +1,6 @@
 #' Install Dependencies on Conda Environments
 #' @export
-install_dependencies <- function(force = FALSE, verbose = "silent") {
-  check_cmd("blastn", env_name = "blast-env", verbose = verbose)
-  check_cmd("efetch", env_name = "entrez-env", verbose = verbose)
+install_dependencies <- function(verbose = "silent", force = FALSE) {
+  check_cmd("blastn", env_name = "blast-env", verbose = verbose, force = force)
+  check_cmd("efetch", env_name = "entrez-env", verbose = verbose, force = force)
 }
