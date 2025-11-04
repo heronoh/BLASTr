7ea35124941dd4245e88cab87e8fd0543b2b543e
luciorq <luciorqueiroz@gmail.com>
2025-11-04 09:09:15 -0500

    tests: split tests for parallel blast implementations

--- tests/testthat/test-parallel_blast_old.R
+++ tests/testthat/test-parallel_blast_old.R
@@ -1,4 +1,4 @@
-testthat::test_that("parallel_blast works", {
+testthat::test_that("new parallel_blast generates same output as old", {
   testthat::skip_on_cran()
   testthat::skip_if_offline()
   ASVs_test <- readLines(fs::path_package(
@@ -10,7 +10,7 @@ testthat::test_that("parallel_blast works", {
 
   db_path <- tmp_blast_db_path
 
-  blast_res <- parallel_blast(
+  blast_res_new <- parallel_blast(
     query_seqs = ASVs_test,
     db_path = db_path,
     total_cores = 1,
@@ -22,8 +22,27 @@ testthat::test_that("parallel_blast works", {
     verbose = "silent"
   )
 
-  testthat::expect_s3_class(blast_res, "tbl_df")
+  blast_res_old <- parallel_blast_old(
+    asvs = ASVs_test,
+    db_path = db_path,
+    out_file = NA,
+    out_RDS = NA,
+    total_cores = 1,
+    perc_id = 80,
+    num_threads = 1,
+    perc_qcov_hsp = 80,
+    num_alignments = 4,
+    blast_type = "blastn",
+    verbose = "silent"
+  )
+
+  testthat::expect_equal(
+    dplyr::select(blast_res_new, -c("1_staxid", "exit_code", "stderr")),
+    dplyr::select(blast_res_old, -c("1_staxid"))
+  )
+  testthat::expect_s3_class(blast_res_new, "tbl_df")
 })
+
 
 testthat::test_that("parallel_blast - query with no hits", {
   testthat::skip_on_cran()
@@ -86,7 +105,7 @@ testthat::test_that("parallel_blast - query that errors", {
 })
 
 
-testthat::test_that("parallel_blast - 2 threads works", {
+testthat::test_that("OLD - parallel_blast works", {
   testthat::skip_on_cran()
   testthat::skip_if_offline()
   ASVs_test <- readLines(fs::path_package(
@@ -98,9 +117,11 @@ testthat::test_that("parallel_blast - 2 threads works", {
 
   db_path <- tmp_blast_db_path
 
-  blast_res <- parallel_blast(
-    query_seqs = ASVs_test,
+  blast_res <- parallel_blast_old(
+    asvs = ASVs_test,
     db_path = db_path,
+    out_file = NA,
+    out_RDS = NA,
     total_cores = 2,
     perc_id = 80,
     num_threads = 1,
@@ -114,7 +135,7 @@ testthat::test_that("parallel_blast - 2 threads works", {
 })
 
 
-testthat::test_that("parallel blast - single thread", {
+testthat::test_that("OLD - parallel blast - single thread", {
   testthat::skip_on_cran()
   testthat::skip_if_offline()
   asvs_test <- readLines(
@@ -123,8 +144,8 @@ testthat::test_that("parallel blast - single thread", {
 
   db_path <- tmp_blast_db_path
 
-  blast_res <- parallel_blast(
-    query_seqs = asvs_test,
+  blast_res <- parallel_blast_old(
+    asvs = asvs_test,
     db_path = db_path,
     num_threads = 1,
     total_cores = 1
@@ -134,18 +155,15 @@ testthat::test_that("parallel blast - single thread", {
   testthat::expect_s3_class(blast_res, "tbl_df")
   testthat::expect_equal(nrow(blast_res), length(asvs_test))
   testthat::expect_equal(nrow(blast_res), 6L)
-  testthat::expect_equal(ncol(blast_res), 59L)
+  testthat::expect_equal(ncol(blast_res), 57L)
 })
 
-testthat::test_that("parallel blast - empty args", {
+testthat::test_that("OLD - parallel blast - empty args", {
   testthat::skip_on_cran()
   testthat::skip_if_offline()
-  parallel_blast(query_seqs = NULL, db_path = NULL) |>
-    expect_error(class = "blastr_no_query_seqs_error")
-
-  parallel_blast(query_seqs = character(0), db_path = NULL) |>
-    expect_error(class = "blastr_no_query_seqs_error")
-
-  parallel_blast(query_seqs = "AAAA", db_path = NULL) |>
-    expect_error(class = "blastr_no_db_path_error")
+  testthat::expect_warning(
+    blast_res <- parallel_blast_old(asvs = NULL, db_path = NULL)
+  )
+  testthat::expect_s3_class(blast_res, "data.frame")
+  testthat::expect_s3_class(blast_res, "tbl_df")
 })
